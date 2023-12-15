185d625a77ceaaedbcc6406a7ac75bdd706acfd1
lingwh <LRwq942133>
2023-12-16 05:33:45 +0800

    vue

--- framework-javascript/vue2/vue-049-vue中的路由/vue-router-004-to-query/src/pages/Message.vue
+++ framework-javascript/vue2/vue-049-vue中的路由/vue-router-004-to-query/src/pages/Message.vue
@@ -3,17 +3,17 @@
     <ul>
       <li v-for="message in messages" :key="message.id">
         <!--
-          使用to传递参数的第一种写法：字符串式写法
+          使用to传递参数 + 使用query接收参数 的第一种写法：字符串式写法
               1.to前面要加:
               2.to后面的内容是用es6语法 `` 和 模板字符串完成传值
               3.模板字符串外面要套一层 '' 或者 “”，否则无法解析
-          使用to传递参数的第一种写法：对象式写法
+          使用to传递参数 + 使用query接收参数 的第一种写法：对象式写法
               1.to前面要加:
               2.to后面的对象中要使用 query 属性封装参数
         -->
         <!--
         <router-link active-class="active" :to="`/home/message/detail?id=${message.id}&content=${message.content}`">
-          to的字符串写法-{{message.title}}
+          使用to传递参数 + 使用query接收参数-字符串式写法-{{message.title}}
         </router-link>
         -->
 
@@ -25,7 +25,7 @@
                   content: message.content
                 }
               }">
-          to的对象写法-{{message.title}}
+          使用to传递参数 + 使用query接收参数-对象式写法{{message.title}}
         </router-link>
       </li>
     </ul>
