4f5a45f371440e952f7c6645ccbaab93c525f52b
lingwh <LRwq942133>
2024-01-05 20:26:29 +0800

    vue

--- framework-javascript/vue3/vue-019-vue3中的suspense/suspense/src/components/Child.vue
+++ framework-javascript/vue3/vue-019-vue3中的suspense/suspense/src/components/Child.vue
@@ -5,7 +5,18 @@
 </template>
 
 <script>
+import {ref} from 'vue'
+
 export default {
   name: 'Child',
+  async setup() {
+    let sum = ref(0)
+    let p = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({sum})
+      }, 3000)
+    })
+    return await p
+  }
 }
 </script>
