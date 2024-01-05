5ed7d422917ac766a875da9617fe85ae9508dfc5
lingwh <LRwq942133>
2024-01-05 20:15:30 +0800

    vue

--- framework-javascript/vue3/vue-019-vue3中的suspense/suspense/src/App.vue
+++ framework-javascript/vue3/vue-019-vue3中的suspense/suspense/src/App.vue
@@ -11,7 +11,10 @@
 </template>
 
 <script>
-/** Suspense等待异步组件时渲染一些额外内容，让应用有更好的用户体验，要配合 defineAsyncComponent 使用 */
+/**
+ * 异步引入组件
+ * Suspense等待异步组件时渲染一些额外内容，让应用有更好的用户体验，要配合 defineAsyncComponent 使用
+ * */
 import {defineAsyncComponent} from 'vue'
 const Child = defineAsyncComponent(()=>import('./components/Child.vue'))
 
