6f6bbacb9c774c4a57797b1d23e4128dc044959d
think <lingwh.walkman@hotmail.com>
2025-01-14 16:52:18 +0800

    修改项目结构

--- 03-JS/03-ES6-13/ES6/27-ES6中的Promise对象_使用案例_封装读取单个文件方法.js
+++ 03-JS/03-ES6-13/ES6/27-ES6中的Promise对象_使用案例_封装读取单个文件方法.js
@@ -5,7 +5,7 @@ const fs = require('fs')
  * 读取文件：不使用Promise封装，容易出现多层嵌套和回调地狱问题
  */
 /*
-fs.readFile('./resource/a.txt',(error,data) => {
+fs.readFile('./resource/为学.md',(error,data) => {
   //如果发生错误，抛出错误
   if(error) throw error
   //没有错误，输出文件内容
@@ -17,7 +17,7 @@ fs.readFile('./resource/a.txt',(error,data) => {
  */
 const promise = new Promise((resolve, reject) => {
   //注意：Promise中封装的是异步函数，而fs.readFile()就是一个异步函数
-  fs.readFile('./resource/a.txt', (error, data) => {
+  fs.readFile('./resource/为学.md', (error, data) => {
     if (error) reject('a.txt文件未找到')
     resolve(data)
   })
--- 03-JS/03-ES6-13/ES6/28-ES6中的Promise对象_使用案例_封装读取多个文件方法.js
+++ 03-JS/03-ES6-13/ES6/28-ES6中的Promise对象_使用案例_封装读取多个文件方法.js
@@ -5,12 +5,12 @@ const fs = require('fs')
  * 读取文件：不使用Promise封装，容易出现多层嵌套、回调地狱问题、变量名称写错问题
  */
 /*
-fs.readFile('./resource/a.txt',(error1,data1) => {
+fs.readFile('./resource/为学.md',(error1,data1) => {
   const aTxt = data1.toString()
-  //没有错误，继续读取b.txt文件内容
-  fs.readFile('./resource/b.txt',(error2,data2) => {
+  //没有错误，继续读取插秧诗.md文件内容
+  fs.readFile('./resource/插秧诗.md',(error2,data2) => {
     const bTxt = data2.toString()
-    fs.readFile('./resource/c.txt',(error3,data3) => {
+    fs.readFile('./resource/观书有感.md',(error3,data3) => {
       const cTxt = data3.toString()
       const allTxt = aTxt + '\r\n' +bTxt + '\r\n' + cTxt
       console.log(allTxt)
@@ -25,8 +25,8 @@ fs.readFile('./resource/a.txt',(error1,data1) => {
 /**/
 const promise = new Promise((resolve, reject) => {
   //注意：Promise中封装的是异步函数，而fs.readFile()就是一个异步函数
-  fs.readFile('./resource/a.txt', (error, data) => {
-    if (error) reject('a.txt文件未找到')
+  fs.readFile('./resource/为学.md', (error, data) => {
+    if (error) reject('为学.md文件未找到')
     resolve(data)
   })
 })
@@ -34,8 +34,8 @@ const promise = new Promise((resolve, reject) => {
 promise.then(value => {
   return new Promise((resolve, reject) => {
     //注意：Promise中封装的是异步函数，而fs.readFile()就是一个异步函数
-    fs.readFile('./resource/b.txt', (error, data) => {
-      if (error) reject('b.txt文件未找到')
+    fs.readFile('./resource/插秧诗.md', (error, data) => {
+      if (error) reject('插秧诗.md文件未找到')
       resolve([value, data])
     })
   })
@@ -45,8 +45,8 @@ promise.then(value => {
   .then(value => {
     return new Promise((resolve, reject) => {
       //注意：Promise中封装的是异步函数，而fs.readFile()就是一个异步函数
-      fs.readFile('./resource/c.txt', (error, data) => {
-        if (error) reject('c.txt文件未找到')
+      fs.readFile('./resource/观书有感.md', (error, data) => {
+        if (error) reject('观书有感.md文件未找到')
         value.push(data)
         resolve(value)
       })
