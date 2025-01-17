d3691da83470e54239cd356fa0449d033a6e60ee
lingwh <lingwh.walkman@homail.com>
2025-01-17 22:37:23 +0800

    修改项目结构

--- 03-JS/03-ES6-13/ES11/js/app.js
+++ 03-JS/03-ES6-13/ES11/js/app.js
@@ -3,6 +3,7 @@
 const btn = document.getElementById('btn')
 
 btn.onclick = function () {
+  //使用import()函数实现动态导入，用的时候才导入，可以实现懒加载效果
   import('./hello.js').then(module => {
     module.hello()
   })
